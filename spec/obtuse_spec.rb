a050af85a06ce6c5856a802a93d513b427c4d48a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-18 18:30:15 +0530

    Organize specs in a better way.

--- spec/obtuse_spec.rb
+++ spec/obtuse_spec.rb
@@ -5,128 +5,116 @@ describe Obtuse do
     Obtuse::VERSION.should be_a String
   end
 
-  describe "Integer" do
-    describe "" do
+  describe "Primitives" do
+    describe "Integer" do
       e "2", 2
       e "100", 100
       e "-10", -10
     end
 
-    describe "#" do
-      e "3#", [0, 1, 2]
+    describe "String" do
+      e %q{"a \n b \n c \t\td\na"}, "a \n b \n c \t\td\na"
     end
   end
 
-  describe "String" do
-    describe "" do
-      e %q{"a \n b \n c \t\td\na"}, "a \n b \n c \t\td\na"
+  describe "Functions" do
+    describe "+" do
+      describe "Integer Integer" do
+        e "2 1+", 3
+        e "10 20 +", 30
+        e "2 -3+", -1
+      end
+
+      describe "String Integer" do
+          e %q{"foo: "42+}, "foo: 42"
+      end
+
+      describe "String String" do
+        e '"foo""bar"+', "foobar"
+        e '"foo" "bar"+', "foobar"
+        e '"foo\\"" "bar"+', 'foo"bar'
+      end
+    end
+
+    describe "-" do
+      describe "Integer Integer" do
+        e "2 3-", -1
+        e "10 20 -", -10
+        e "-10 -20-", 10
+      end
+
+      describe "String String" do
+        e %q{"aaa""a"-}, ""
+        e %q{"cabc""b"-}, "cac"
+        e %q{"cabc""cb"-}, "a"
+        e %q{"cabc""cab"-}, ""
+      end
+    end
+
+    describe "*" do
+      describe "Integer Integer" do
+        e "2 3*", 6
+        e "10 20 *", 200
+        e "-5 5*", -25
+      end
+
+      describe "String Integer" do
+        e %q{"foobar "3*}, "foobar foobar foobar "
+      end
+    end
+
+    describe "/" do
+      describe "Integer Integer" do
+        e "2 3/", 0
+        e "10 20 /", 0
+        e "10 -2/", -5
+      end
+    end
+
+    describe "%" do
+      describe "Integer Integer" do
+        e "2 3%", 2
+        e "10 20 %", 10
+        e "5 -3%", -1
+      end
+
+      describe "String Integer" do
+        e %q{"%05d" 123%}, "00123"
+      end
+
+      describe "String Array" do
+        e %q{"%02d %03d %04d"4#%}, "00 001 0002"
+      end
+    end
+
+    describe "^" do
+      describe "Integer Integer" do
+        e "2 3^", 8
+        e "10 20 ^", 10 ** 20
+        e "-3 3^", -27
+      end
     end
 
     describe "#" do
-      e %q{"foo bar"#}, 7
+      describe "Integer" do
+        e "3#", [0, 1, 2]
+      end
+
+      describe "String" do
+        e %q{"foo bar"#}, 7
+      end
+
+      describe "Array" do
+        e "10##", 10
+      end
     end
 
     describe "$" do
-      e %q{"abcdcba"$}, "aabbccd"
-    end
-  end
-
-  describe "Integer Integer" do
-    describe "+" do
-      e "2 1+", 3
-      e "10 20 +", 30
-      e "2 -3+", -1
+      describe "String" do
+        e %q{"abcdcba"$}, "aabbccd"
+      end
     end
 
-    describe "-" do
-      e "2 3-", -1
-      e "10 20 -", -10
-      e "-10 -20-", 10
-    end
-
-    describe "*" do
-      e "2 3*", 6
-      e "10 20 *", 200
-      e "-5 5*", -25
-    end
-
-    describe "/" do
-      e "2 3/", 0
-      e "10 20 /", 0
-      e "10 -2/", -5
-    end
-
-    describe "%" do
-      e "2 3%", 2
-      e "10 20 %", 10
-      e "5 -3%", -1
-    end
-
-    describe "^" do
-      e "2 3^", 8
-      e "10 20 ^", 10 ** 20
-      e "-3 3^", -27
-    end
-
-    describe "Combination" do
-      e "1 2+3-", 0
-      e "5 1 4 5 2 3 4 +-+-+-", 0
-      e "1-2-3-+", 2
-      e "1-2- 3+", 6
-    end
-  end
-
-  describe "String String" do
-    describe "+" do
-      e '"foo""bar"+', "foobar"
-      e '"foo" "bar"+', "foobar"
-      e '"foo\\"" "bar"+', 'foo"bar'
-    end
-
-    describe "-" do
-      e %q{"aaa""a"-}, ""
-      e %q{"cabc""b"-}, "cac"
-      e %q{"cabc""cb"-}, "a"
-      e %q{"cabc""cab"-}, ""
-    end
-  end
-
-  describe "String Integer" do
-    describe "+" do
-      e %q{"foo: "42+}, "foo: 42"
-    end
-
-    describe "-" do
-    end
-
-    describe "*" do
-      e %q{"foobar "3*}, "foobar foobar foobar "
-    end
-
-    describe "/" do
-    end
-
-    describe "%" do
-      e %q{"%05d" 123%}, "00123"
-    end
-
-    describe "^" do
-    end
-  end
-
-  describe "String Array" do
-    describe "%" do
-      e %q{"%02d %03d %04d"4#%}, "00 001 0002"
-    end
-  end
-
-  describe "Array" do
-    describe "#" do
-      e "10##", 10
-    end
-  end
-
-  describe "" do
     describe "Ia" do
       e "Ia", "abc\nbcd", "abc\nbcd"
     end
@@ -134,5 +122,14 @@ describe Obtuse do
     describe "Il" do
       e "Il", "abc", "abc\nbcd"
     end
+  end
+
+
+
+  describe "Mixture" do
+    e "1 2+3-", 0
+    e "5 1 4 5 2 3 4 +-+-+-", 0
+    e "1-2-3-+", 2
+    e "1-2- 3+", 6
   end
 end
