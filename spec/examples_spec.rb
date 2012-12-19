905f310f65c0967bc97c15b9965096c632b98b59
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-19 15:49:51 +0530

    Add solution for Project Euler Problem #1.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -15,6 +15,10 @@ describe Obtuse do
   end
 
   describe "Project Euler" do
+    describe "Problem #1: Sum of all the multiples of 3 or 5 below 1000." do
+      e '1000#{{.3%}{{.5%}{;}{}I}{}I}%{+}*', 233168
+    end
+
     describe "Problem #6: Find the difference between the sum of the squares" +
       " of the first one hundred natural numbers and the square of the sum." do
       e '101#{+}*2^101#{2^}%{+}*-', 25164150
