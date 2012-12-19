b94c54ce39ec9fb3ac228a4c7d4b01a3c765ad2a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-19 11:24:26 +0530

    Add GCD example.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -1,6 +1,11 @@
 require "spec_helper"
 
 describe Obtuse do
+  describe "GCD" do
+    # From http://www.golfscript.com/golfscript/examples.html
+    e '13483 74205 {.}{.@\%}W;', 97
+  end
+
   describe "Project Euler" do
     describe "Problem #6: Find the difference between the sum of the squares" +
       " of the first one hundred natural numbers and the square of the sum." do
