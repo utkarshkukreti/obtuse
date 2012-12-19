ddf3896c282476ec64b35bbd220804042ad2653a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-20 00:01:03 +0530

    Add solution for Project Euler Problem #28.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -37,5 +37,23 @@ describe Obtuse do
       " more than 1000 digits" do
       e '[1 1{.Ts#1000<}{.@.@+@\}W]#', 4782
     end
+
+    describe <<-EOT do
+Problem #28: Starting with the number 1 and moving to the right in a clockwise \
+direction a 5 by 5 spiral is formed as follows:
+
+                               *21*  22 23   24 *25*
+                                20  *7*  8  *9*  10
+                                19   6  *1*  2   11
+                                18  *5*  4  *3*  12
+                               *17*  16 15   14 *13*
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed\
+ in the same way?
+    EOT
+      e '[1[1 500#{1+2*}%{...}%~]{+.}*;]{+}*', 669171001
+    end
   end
 end
