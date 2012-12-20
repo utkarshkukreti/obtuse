b341a7e6f636fe1fdba49fcbac9f39bd9e01d234
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-21 01:26:21 +0530

    Implement `Ib` on (Integer | String) Integer (Converts x to base y).

--- lib/obtuse/functions/integer.rb
+++ lib/obtuse/functions/integer.rb
@@ -11,5 +11,13 @@ module Obtuse
     fn :Ir, String do |x|
       push rand(x.to_i == 0 ? 2 ** 32 : x.to_i)
     end
+
+    fn :Ib, Integer, Integer do |x, y|
+      push x.to_s(y)
+    end
+
+    fn :Ib, String, Integer do |x, y|
+      push x.to_i.to_s(y)
+    end
   end
 end
--- spec/obtuse/functions/integer_spec.rb
+++ spec/obtuse/functions/integer_spec.rb
@@ -12,4 +12,13 @@ describe Obtuse do
       # No way to this with the current spec helpers.
     end
   end
+
+  describe "Ib" do
+    describe "Object Object" do
+      e %q{10 2 Ib}, "1010"
+      e %q{64 16 Ib}, "40"
+      e %q{"10" 11 Ib}, "a"
+      e %q{"90" 20 Ib}, "4a"
+    end
+  end
 end
