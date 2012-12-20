3da6da31cff51e78cf1464b765ac8cba09cfc669
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-21 00:33:22 +0530

    Add support for `/` on (String | Array) Integer (slice into chunks).

--- lib/obtuse/functions/slash.rb
+++ lib/obtuse/functions/slash.rb
@@ -3,5 +3,13 @@ module Obtuse
     fn :/, Integer, Integer do |x, y|
       push x / y
     end
+
+    fn :/, String, Integer do |x, y|
+      push x.chars.to_a.each_slice(y).map(&:join)
+    end
+
+    fn :/, Array, Integer do |x, y|
+      push x.each_slice(y).to_a
+    end
   end
 end
--- spec/obtuse/functions/slash_spec.rb
+++ spec/obtuse/functions/slash_spec.rb
@@ -7,5 +7,10 @@ describe Obtuse do
       e "10 20 /", 0
       e "10 -2/", -5
     end
+
+    describe "(String | Array) Integer" do
+      e %q{"foobar foob" 3 /}, ["foo", "bar", " fo", "ob"]
+      e %q{10# 3 /}, [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+    end
   end
 end
