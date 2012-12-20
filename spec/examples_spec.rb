b272b8959de963fae4b2ff831e7917a204196437
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-20 16:47:53 +0530

    Add a shorter solution to Project Euler Problem #1, using Array filter.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -18,6 +18,7 @@ describe Obtuse do
     describe "Problem #1: Sum of all the multiples of 3 or 5 below 1000." do
       e '1000#{{.3%}{{.5%}{;}{}I}{}I}%{+}*', 233168
       e '1000#{..3%!\5%!|{}{;}I}%{+}*', 233168
+      e '1000#{.3%!\5%!|}|{+}*', 233168
     end
 
     describe "Problem #6: Find the difference between the sum of the squares" +
