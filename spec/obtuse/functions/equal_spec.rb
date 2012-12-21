91cd45dbda66004669114359d3ca5fd4063777c1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-22 01:06:30 +0530

    Fix spec description for `=`.

--- spec/obtuse/functions/equal_spec.rb
+++ spec/obtuse/functions/equal_spec.rb
@@ -2,9 +2,16 @@ require "spec_helper"
 
 describe Obtuse do
   describe "=" do
-    describe "Object Object" do
+    describe "Integer Integer" do
       e %q{12 12 =}, 1
+    end
+
+    describe "String String" do
       e %q{"foo" "foo " =}, 0
+    end
+
+    describe "Array Array" do
+      e %q{5# [0 1 2 3 4] =}, 1
     end
   end
 end
